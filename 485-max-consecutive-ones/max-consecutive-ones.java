f2ca9d2822cb4a85179f90eb328402474d3b5f74
Harshit Kapoor <51022801+Harshit-Kapoor@users.noreply.github.com>
2024-08-10 12:07:58 +0530

    Time: 2 ms (88.56%) | Memory: 46 MB (6.48%) - LeetSync

--- 485-max-consecutive-ones/max-consecutive-ones.java
+++ 485-max-consecutive-ones/max-consecutive-ones.java
@@ -20,4 +20,29 @@ class Solution {
         return maxCount;
         
     }
+    
+    public int findMaxConsecutiveOnes1(int[] arr) {
+        
+        int finalAnswer = 0;
+        int n = arr.length;
+
+        for (int l = 0; l < n; ++l) {
+            
+            int no_of_zeroes = 0;
+            
+            for (int r = l; r < n; ++r) {
+                if (arr[r] == 0) {
+                    no_of_zeroes++;
+                }
+
+                if (no_of_zeroes == 0) {
+                    finalAnswer = Math.max(finalAnswer, r - l + 1);
+                }
+            }
+        }
+
+        return finalAnswer;
+
+        
+    }
 }
