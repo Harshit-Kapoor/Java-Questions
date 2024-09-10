87dc403b0d39159d2ecc2aabce32b1271947a9c9
Harshit Kapoor <51022801+Harshit-Kapoor@users.noreply.github.com>
2024-09-10 15:15:24 +0530

    Time: 272 ms (7.67%) | Memory: 58.6 MB (64.57%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.java
+++ 239-sliding-window-maximum/sliding-window-maximum.java
@@ -1,32 +1,30 @@
 class Solution {
-    public int[] maxSlidingWindow(int[] nums, int k) {
-        
-        int j=0,i=0,maxsum=Integer.MIN_VALUE,arrayIndex=0;
-        int[] x=new int[nums.length-k+1];
-        List<Integer> f=new ArrayList<>();
-        
-        
-        while(j<nums.length){
-            
-            while(f.size()!=0 && f.get(f.size()-1)<nums[j]){
-                f.remove(f.size()-1);
+    public static int[] maxSlidingWindow(int[] arr, int k) {
+
+        int j = 0, i = 0, arrayIndex = 0;
+        int[] ans = new int[arr.length - k + 1];
+        List<Integer> l = new ArrayList<>();
+
+        while (j < arr.length) {
+
+            while (l.size() != 0 && l.get(l.size() - 1) < arr[j]) {                                // calculation - ek list bana lenge, and list mein sirf kaam ke elemets ayenge, means arr[j] se pehle jo bhi smaller hai wo remove krdo because those would never be used, and if when we remove arr[j] to j ke right side waale elements mein hi next max hoga
+                l.remove(l.size() - 1);
             }
-            f.add(nums[j]);
-            
-            
-            if(j-i+1<k){
+            l.add(arr[j]);
+
+            if (j - i + 1 < k) {
                 j++;
-            }
-            else if(j-i+1==k){
-                x[arrayIndex]=f.get(0);
+            } else if (j - i + 1 == k) {
+                ans[arrayIndex] = l.get(0);
                 arrayIndex++;
-                if(nums[i]==f.get(0))f.remove(0);
+                if (arr[i] == l.get(0)) l.remove(0);
                 i++;
                 j++;
-                
+
             }
         }
-        return x;
-        
+        return ans;
+
+
     }
 }
