3e5e0f6b62f637d38c6de8980b8b234b1f41cf59
Harshit Kapoor <51022801+Harshit-Kapoor@users.noreply.github.com>
2024-11-02 23:49:36 +0530

    Time: 2 ms (79.47%) | Memory: 53.5 MB (9.24%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,5 +1,46 @@
 class Solution {
+    
     public int majorityElement(int[] nums) {
+        
+// Approach for Moore's algorithm
+// Initialize two variables: count and candidate. Set count to 0 and candidate to an arbitrary value that is 0 in       this case.
+// Iterate through the array nums:
+// a. If count is 0, assign the current element as the new candidate and increment count by 1.
+// b. If the current element is the same as the candidate, increment count by 1.
+// c. If the current element is different from the candidate, decrement count by 1.
+// After the iteration, the candidate variable will hold the majority element.
+        
+        int n = nums.length;
+        int element = nums[0];
+        int freq = 1;
+        for (int i = 1; i < n; i++) {
+            if (freq == 0) {
+                element = nums[i];
+                freq = 1;
+            } else if (nums[i] == element) {
+                freq++;
+            } else {
+                freq = freq - 1;
+            }
+        }
+
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == element) {
+                count++;
+
+            }
+        }
+
+        int result = 0;
+        if (count > n / 2) {
+            result = element;
+        }
+
+        return result;        
+    }
+    
+    public int majorityElement1(int[] nums) {
         
         HashMap<Integer, Integer> map= new HashMap<>();
         
