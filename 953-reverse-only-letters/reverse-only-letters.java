0196f296c34a5cb650f01f6036fcdc519a13e225
Harshit Kapoor <51022801+Harshit-Kapoor@users.noreply.github.com>
2024-12-05 21:36:33 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (48.69%) - LeetSync

--- 953-reverse-only-letters/reverse-only-letters.java
+++ 953-reverse-only-letters/reverse-only-letters.java
@@ -1,23 +1,42 @@
 class Solution {
+    
+public static boolean isEnglishLetter(char c) {
+    
+    if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122)) {
+        return true;
+    }
+    
+    return false;
+    
+}    
+    
 public static String reverseOnlyLetters(String s) {
-        char[] charArray = s.toCharArray();
-        int i = 0, j = s.length() - 1;
-
-        while (i <= j) {
-            if (!Character.isLetter(charArray[i])) {
-                i++;
-            }
-            else if (!Character.isLetter(charArray[j])) {
-                j--;
-            }
-            else {
-                char temp = charArray[i];
-                charArray[i] = charArray[j];
-                charArray[j] = temp;
-                i++;
-                j--;
-            }
+    
+    char[] c = s.toCharArray();
+    
+    int start = 0;
+    int end = c.length - 1;
+    
+    while (start < end) {
+        
+        if (isEnglishLetter(c[start]) == true && isEnglishLetter(c[end]) == true) {
+            char temp = c[start];
+            c[start] = c[end];
+            c[end] = temp;
+            start++;
+            end--;
+        } else if (isEnglishLetter(c[start]) == true && isEnglishLetter(c[end]) == false) {
+            end--;
+        } else if (isEnglishLetter(c[start]) == false && isEnglishLetter(c[end]) == true) {
+            start++;
+        } else {
+            start++;
+            end--;
         }
-        return new String(charArray);
+                    
+    }
+    
+    return new String(c);
+    
     }
 }
