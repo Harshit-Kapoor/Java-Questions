5b6918bce20f39abe83d7e9c89dd189862597e07
Harshit Kapoor <51022801+Harshit-Kapoor@users.noreply.github.com>
2025-03-06 20:17:49 +0530

    Time: 32 ms (8.95%) | Memory: 75.3 MB (36.42%) - LeetSync

--- 2465-shifting-letters-ii/shifting-letters-ii.java
+++ 2465-shifting-letters-ii/shifting-letters-ii.java
@@ -1,104 +1,63 @@
 class Solution {
-/*    
-        public static String shiftingLetters(String s, int[][] shifts) {
 
+    public static String shiftingLetters(String s, int[][] shifts) {
+
+        int n = s.length();
+
+        int[] differenceArr = new int[n];
+
+        for (int[] shift : shifts) {
+
+            int leftIndex = shift[0];
+            int rightIndex = shift[1];
+            int direction = shift[2];
+
+            // if direction is 1, we have to move shift char forward, so our x value will be +1,
+            // so as per our diff array logic we add x to leftIndex and minus it at r+1 index
+
+            // if direction is 0, we have to move shift char back, so our x value will be -1,
+            // so as per our diff array logic we add x to leftIndex ie -1 and minus it at r+1 index ie - of -1
+
+            int x;
+
+            if (direction == 1) {
+                x = 1;
+            } else {
+                x = -1;
+            }
+
+            differenceArr[leftIndex] += x;
+            if (rightIndex + 1 < n) {
+                differenceArr[rightIndex + 1] -= x;
+            }
+
+        }
+
+        System.out.println(Arrays.toString(differenceArr));
+
+        // prefix
+
+        for (int i=1; i<differenceArr.length; i++) {
+            differenceArr[i] = differenceArr[i - 1] + differenceArr[i];
+        }
+
+        System.out.println(Arrays.toString(differenceArr));
 
         char[] ch = s.toCharArray();
 
-        for (int i = 0; i < shifts.length; i++) {
+        for (int i=0; i < differenceArr.length; i++) {
 
-            int left = shifts[i][0];
-            int right = shifts[i][1];
-            int direction = shifts[i][2];
+            int shift = differenceArr[i] % 26;  // wrap z - a
 
-            for (int j = left; j <= right; j++) {
-
-                if (direction == 1) {
-
-                    ch[j] = (char) (ch[j] + 1);
-
-                    if (ch[j] > 122) {
-                        ch[j] -= 26;
-                    } else if (ch[j] < 97) {
-                        ch[j] += 26;
-                    }
-
-
-                } else {
-
-                    ch[j] = (char) (ch[j] - 1);
-
-                    if (ch[j] > 122) {
-                        ch[j] -= 26;
-                    } else if (ch[j] < 97) {
-                        ch[j] += 26;
-                    }
-                }
-
+            if (shift < 0) {
+                shift += 26;   // wrap
             }
 
+            ch[i] = (char) ((((ch[i] - 'a') + shift) % 26) + 'a');
+
         }
 
-        String ans = "";
+        return String.valueOf(ch);
 
-        for (int i = 0; i < ch.length; i++) {
-            ans += ch[i];
-        }
-
-        return ans;
-
-    }
-    
-*/
-        
-/*
-    We will perform total three passes:
-
-    In first pass we traverse shifts vector: we will put value in vector,
-    if(shifts[i][2]==1), then do +=1 at shifts[i][0] index & -=1 at shifts[i][0]+1 index of vector(prefixsum)
-    else ,do -=1 at shifts[i][0] index & +=1 at shifts[i][0]+1 index of vector(prefixsum)
-
-    In second pass we traverse vector and do prefixsum to find displacement of character at index i
-
-    In third pass we traverse string and shift the character ((s[i]-'a')+vec[i])%26 places from 'a'
-*/
-    
-        
-        public String shiftingLetters(String s, int[][] shifts) {
-        int n = s.length();
-        int[] vec = new int[n + 1];
-
-        // Fill vec with cumulative shift values
-        for (int[] shift : shifts) {
-            int start = shift[0];
-            int end = shift[1];
-            int dir = shift[2];
-            if (dir == 1) {
-                vec[start]++;
-                vec[end + 1]--;
-            } else {
-                vec[start]--;
-                vec[end + 1]++;
-            }
-        }
-
-        // Calculate prefix sum for cumulative shifts
-        for (int i = 1; i <= n; i++) {
-            vec[i] += vec[i - 1];
-        }
-
-        // Shift each character and wrap around alphabet
-        char[] charArray = s.toCharArray();
-        for (int i = 0; i < n; i++) {
-            int shiftedIndex = (charArray[i] - 'a' + vec[i]) % 26;
-            if (shiftedIndex < 0) {
-                shiftedIndex += 26;
-            }
-            charArray[i] = (char) ('a' + shiftedIndex);
-        }
-
-        return new String(charArray);
-    }
-
-
+    }    
 }
