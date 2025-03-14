c6e7c21e21084b2b0e76209ef6dfb298b18761f6
Harshit Kapoor <51022801+Harshit-Kapoor@users.noreply.github.com>
2025-03-15 00:33:03 +0530

    Time: 2 ms (81.59%) | Memory: 42.2 MB (67.53%) - LeetSync

--- 1666-make-the-string-great/make-the-string-great.java
+++ 1666-make-the-string-great/make-the-string-great.java
@@ -1,5 +1,25 @@
 class Solution {
-    public String makeGood(String s) {
+
+public static String makeGood(String s) {
+
+        StringBuffer sb = new StringBuffer();
+        sb.append(s.charAt(0));
+
+        for (int i=1; i<s.length(); i++) {
+
+            if (sb.length() > 0 && ((s.charAt(i) == sb.charAt(sb.length() - 1) - 32) || (s.charAt(i) == sb.charAt(sb.length() - 1) + 32))) {
+                sb.deleteCharAt(sb.length() - 1);
+            } else {
+                sb.append(s.charAt(i));
+            }
+
+        }
+
+        return sb.toString();
+
+    }
+
+    public String makeGood1(String s) {
         Stack<Character>st=new Stack<>();
         for(int i=0;i<s.length();i++){
             char ch=s.charAt(i);
