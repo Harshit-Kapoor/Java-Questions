1bfc77b8ab76950b38c0ed5e7b7eadb9f55cc44c
Harshit Kapoor <51022801+Harshit-Kapoor@users.noreply.github.com>
2025-03-23 01:42:53 +0530

    Time: 4 ms (4.23%) | Memory: 43.8 MB (5.93%) - LeetSync

--- 31-next-permutation/next-permutation.java
+++ 31-next-permutation/next-permutation.java
@@ -1,28 +1,44 @@
 class Solution {
-        public void nextPermutation(int[] nums) {
-        int i = nums.length - 2;
+    public static void nextPermutation(int[] nums) {
 
-        // Find first index i from right s.t.
-        // nums[i + 1]  > nums[i]
-        while (i >= 0 && nums[i + 1] <= nums[i]) {
-            i--;
-        }
-        if (i >= 0) {
-            int j = nums.length - 1;
-            // Find j s.t. nums[j] is
-            // just larger than nums[i]
-            while (nums[j] <= nums[i]) {
-                j--;
+        int n = nums.length;
+
+        // first find blue index
+
+        int blueIndex = -1;
+
+        for (int i=n-1; i>0; i--) {
+            if (nums[i-1] < nums[i]) {
+                blueIndex = i - 1;
+                break;
             }
-            swap(nums, i, j);
         }
-        // Make nums[i+1...]
-        // equal to the smallest
-        // possible permutation by reversing
-        reverse(nums, i + 1);
+
+        // check just greater element on the right side
+
+        if (blueIndex != -1) {
+            int swapIndex = blueIndex;
+
+            for (int i = n - 1; i >= blueIndex; i--) {
+                if (nums[i] > nums[blueIndex]) {
+                    swapIndex = i;
+                    break;
+                }
+            }
+
+            swap(nums, blueIndex, swapIndex);
+
+            reverse(nums, blueIndex + 1);
+        } else {
+            reverse(nums, 0);            
+        }
+
+        System.out.println(Arrays.toString(nums));;
+
     }
 
-    private void reverse(int[] nums, int start) {
+
+    private static void reverse(int[] nums, int start) {
         int i = start, j = nums.length - 1;
         while (i < j) {
             swap(nums, i, j);
@@ -31,7 +47,7 @@ class Solution {
         }
     }
 
-    private void swap(int[] nums, int i, int j) {
+    private static void swap(int[] nums, int i, int j) {
         int temp = nums[i];
         nums[i] = nums[j];
         nums[j] = temp;
