5d7f9cc8284c5239fda26186941f7af8865322ff
Harshit Kapoor <51022801+Harshit-Kapoor@users.noreply.github.com>
2025-04-02 01:27:41 +0530

    Time: 230 ms (5.38%) | Memory: 47.6 MB (5.31%) - LeetSync

--- 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.java
+++ 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.java
@@ -1,37 +1,31 @@
 class Solution {
     public static String removeDuplicates(String s) {
 
-        Stack<Character> st = new Stack<Character>();
+        Stack<Character> st = new Stack<>();
 
-        st.push(s.charAt(0));
+        for (char c : s.toCharArray()) {
 
-        for (int i=1; i<s.length(); i++) {
-
-            if (st.size() == 0) {
-                st.push(s.charAt(i));
-                continue;
-            }
-
-            if (st.peek() == s.charAt(i)) {
+            if (!st.isEmpty() && st.peek() == c) {
                 st.pop();
             } else {
-                st.push(s.charAt(i));
+                st.push(c);
             }
 
+        }
+
+        String initial = "";
+
+        while (!st.isEmpty()) {
+            initial += st.pop();
         }
 
         String ans = "";
-        String rev = "";
 
-        while (st.size() > 0) {
-            ans += st.pop();
+        for (int i=initial.length()-1; i>=0; i--) {
+            ans += initial.charAt(i);
         }
 
-        for (int i=ans.length()-1; i>=0; i--) {
-            rev += ans.charAt(i) + "";
-        }
-
-        return rev;
-
+        return ans;
+ 
     }
 }
