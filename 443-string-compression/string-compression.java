10623100a3db46c673169c71f89db43beaa7d26d
Harshit Kapoor <51022801+Harshit-Kapoor@users.noreply.github.com>
2025-04-07 15:09:25 +0530

    Time: 1 ms (99.58%) | Memory: 43.9 MB (79.94%) - LeetSync

--- 443-string-compression/string-compression.java
+++ 443-string-compression/string-compression.java
@@ -5,27 +5,27 @@ class Solution {
         int currIndex = 0;
         int index = 0;
 
-        while (currIndex < chars.length){
+        while (currIndex < chars.length) {
 
             char currChar = chars[currIndex];
             int count = 0;
 
-            // to find duplicates
             while (currIndex < chars.length && currChar == chars[currIndex]) {
-                count++;
                 currIndex++;
+                count++;
             }
 
-            // now do the assigning part
             chars[index++] = currChar;
             if (count > 1) {
+
                 for (char c : String.valueOf(count).toCharArray()) {
                     chars[index++] = c;
                 }
-            } 
+
+            }
 
         }
-     
+
         return index;
 
     }
