bd6aa587c2068bc333de912759277afdf66d859c
Harshit Kapoor <51022801+Harshit-Kapoor@users.noreply.github.com>
2025-04-09 22:32:53 +0530

    Time: 12 ms (15.11%) | Memory: 44.6 MB (76.24%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -1,32 +1,37 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        
+
         int i = 0;
         int j = 0;
-        int max = 0;
         int n = s.length();
-        char[] ch = s.toCharArray();
-        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+        int max = 0;
+
+        HashMap<Character, Integer> map = new HashMap<>();
 
         while (j < n) {
 
-            map.put(ch[j], map.getOrDefault(ch[j], 0) + 1);
+            char c = s.charAt(j);
 
-            if (map.size() > j - i + 1) {          // this condition will never hit as unique char in window cant be > size
-                j++;
-            } else if (map.size() == j - i + 1) {
-                max = Math.max(max, j - i + 1);
-                j++;
-            } else if (map.size() < j - i + 1) {            // here we are reducing and iterating i when its < j-i+1 (since if map has 2 size a->1, b->2, and j-i+1 is 3, so theres a duplicate in window so needs to be removed by iterate=ing i)
-                while (map.size() < j - i + 1) {
-                    map.put(ch[i], map.getOrDefault(ch[i], 0) - 1);
-                    if (map.get(ch[i]) == 0) {
-                        map.remove(ch[i]);
-                    }
-                    i++;
+            map.put(c, map.getOrDefault(c, 0) + 1);
+
+            while (map.get(c) > 1) {
+
+                char charAtI = s.charAt(i);
+
+                if (map.get(charAtI) == 1) {
+                    map.remove(charAtI);
+                } else if (map.get(charAtI) > 1) {
+                    map.put(charAtI, map.get(charAtI) - 1);                    
                 }
-                j++;
+
+                i++;
             }
+
+
+            max = Math.max(max, j - i + 1);
+
+            j++;
+
         }
         
         return max;
